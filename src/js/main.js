e030443c0ba4cf13c2d1c85e654099042a0b344f
nechiporenko <s.nechiporenko@gmail.com>
2017-02-22 12:50:54 +0200

    мелкие правки, комментарии

--- src/js/main.js
+++ src/js/main.js
@@ -1,92 +1,99 @@
-﻿'use strict';
+﻿(function () {
+    'use strict';
 
-var Carousel = (function () {
-    var method = function (list) {
-        this.slider = list;
-        this.slides = this.slider.getElementsByTagName('li');
-        this.count = this.slides.length;
-        this.size = this.slider.getAttribute('data-size');
-        this.timeout = this.slider.getAttribute('data-time');
-        this.mode = this.slider.getAttribute('data-mode');
-        this.current = 0;
+    var Carousel = (function () {
+        var method = function (list) {
+            this.slider = list;//список слайдов
+            this.slides = this.slider.getElementsByTagName('li');//слайды
+            this.count = this.slides.length;//кол-во слайдов
+            this.size = this.slider.getAttribute('data-size');//высота (или ширина) слайда
+            this.delay = this.slider.getAttribute('data-time');//время смены слайдов
+            this.mode = this.slider.getAttribute('data-mode'); //режим (вертикальный - vertical, горизонтальный - horizontal )
+            this.current = 0; //текущий слайд
+        };
+
+        method.prototype.init = function () {
+            //проверим переданные параметры
+            if (isNaN(this.size)) {//если не указали высоту (или ширину) слайда
+                this.size = 100;
+            }
+
+            if (isNaN(this.delay) || this.delay < 1000) {//если забыли указать время смены слайдов
+                this.delay = 2000;
+            }
+
+            if (!(this.mode === 'vertical' || this.mode === 'horizontal')) {//если забыли указать режим
+                this.mode = 'vertical';
+            }
+
+            //поставим последний элемент перед первым
+            var last = this.slides[this.count - 1];
+            this.slider.insertBefore(last, this.slides[0]);
+
+            if (this.mode === 'horizontal') {
+                //сместим список влево на ширину элемента
+                this.slider.style.left = -this.size + 'px';
+                //зададим ширину списка по кол-ву элементов
+                this.slider.style.width = this.size * this.count + 'px';
+            } else {
+                //сместим список на высоту элемента
+                this.slider.style.top = -this.size + 'px';
+            }
+
+            //теперь запускаем с интервалом delay
+            setInterval(this.move.bind(this), this.delay);
+        };
+
+        method.prototype.move = function () {
+            //анимируем и смещаем список на один элемент
+            addClass(this.slider, 'moved');
+            if (this.mode === 'horizontal') {
+                this.slider.style.transform = 'translateX( -' + this.size + 'px)';
+            } else {
+                this.slider.style.transform = 'translateY( -' + this.size + 'px)';
+            }
+            //через 1с (1с - время анимации при смене слайда) поставим первый элемент в конец списка
+            setTimeout(this.change.bind(this), 1000);
+        };
+
+        method.prototype.change = function () {
+            removeClass(this.slider, 'moved'); //отключаем эффект анимации для transform
+            this.slider.style.transform = 'none';
+            this.slider.appendChild(this.slides[0]);//перестановка
+        };
+
+        return method;
+    })();
+
+    //helpers (добавим/уберем класс с fallback для ie9)
+    function addClass(el, className) {
+        if (el.classList)
+            el.classList.add(className);
+        else
+            el.className += ' ' + className;
     };
 
-    method.prototype.init = function () {
-        //проверим переданные параметры
-        if (isNaN(this.size)) {
-            this.size = 100;
-        }
-
-        if (isNaN(this.timeout) || this.timeout < 1000) {
-            this.timeout = 2000;
-        }
-
-        if (!(this.mode === 'vertical' || this.mode === 'horizontal')) {
-            this.mode = 'vertical';
-        }
-
-        //поставим последний элемент перед первым
-        var last = this.slides[this.count - 1];
-        this.slider.insertBefore(last, this.slides[0]);
-
-        if (this.mode === 'horizontal') {
-            //сместим список влево на ширину элемента
-            this.slider.style.left = -this.size + 'px';
-            //зададим ширину списка по кол-ву элементов
-            this.slider.style.width = this.size * this.count + 'px';
-        } else {
-            //сместим список на высоту элемента
-            this.slider.style.top = -this.size + 'px';
-        } 
-
-        //запускаем
-        setInterval(this.move.bind(this), this.timeout);
+    function removeClass(el, className) {
+        if (el.classList)
+            el.classList.remove(className);
+        else
+            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
     };
 
-    method.prototype.move = function () {
-        //анимируем и смещаем список на один элемент
-        addClass(this.slider, 'moved');
-        if (this.mode === 'horizontal') {
-            this.slider.style.transform = 'translateX( -' + this.size + 'px)';
-        } else {
-            this.slider.style.transform = 'translateY( -' + this.size + 'px)';
-        }
-        //через 1с, поставим первый элемент в конец списка
-        setTimeout(this.change.bind(this), 1000);
-    };
 
-    method.prototype.change = function () {
-        removeClass(this.slider, 'moved'); //отключаем эффект анимации
-        this.slider.style.transform = 'none';
-        this.slider.appendChild(this.slides[0]);
-    };
+    //подключаем слайдеры на странице, параметры передаем через data-атрибуты (см. html-разметку)
+    var slider1 = new Carousel(document.getElementById('crsl01'));
+    slider1.init();
 
-    return method;
+    var slider2 = new Carousel(document.getElementById('crsl02'));
+    slider2.init();
+
+    var slider3 = new Carousel(document.getElementById('crsl03'));
+    slider3.init();
 })();
 
 
-//подключаем
-var slider1 = new Carousel(document.getElementById('crsl01'));
-slider1.init();
-
-var slider2 = new Carousel(document.getElementById('crsl02'));
-slider2.init();
-
-var slider3 = new Carousel(document.getElementById('crsl03'));
-slider3.init();
 
 
-//helpers
-function addClass(el, className) {
-    if (el.classList)
-        el.classList.add(className);
-    else
-        el.className += ' ' + className;
-}
 
-function removeClass(el, className) {
-    if (el.classList)
-        el.classList.remove(className);
-    else
-        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-}
+
